fa58c542a3d39efd4573eab07622bd3c8dfe0570
AnikSta <143994219+Annniiikkk@users.noreply.github.com>
2026-08-17 10:53:08 +0200

    init project

---
+++ Ticket_System/src/test/java/ch/tbz/ticket_system/TicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.tbz.ticket_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
